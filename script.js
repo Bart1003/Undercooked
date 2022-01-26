3c2f4192ac11ede60378b63383f98b74bcd81868
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2022-01-26 15:52:08 +0000

    De bal kan nu ook naar links en rechts springen, als voordat je op spatie klikt het linker of rechter pijltje ingedrukt houdt

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Character{
     this.c = color;
     this.v = 1;
     this.a = 0.33; 
+    this.v_hor = 1
   }
 
   
@@ -20,34 +21,39 @@ class Character{
     fill(this.c)
     ellipse(this.x, this.y, this.w, this.h);
 
-    
-    this.y += this.v;
-    this.x += 2
 
     if (this.v < 15){
         this.v = this.v + this.a;
       } 
+    this.y += this.v;
     
+    this.x += this.v_hor
 
-    //helps see what happens to the velocity, not a part of the end game (at least not planned yet, could be fun though)
+    //helps see what happens to the velocities, not a part of the end game (at least not planned yet, could be fun though)
     textSize(32)
     fill(50)
     text(this.v, 100, 30);
+    text(this.v_hor, 100, 70)
 
     //collision met de grond en plafond
     if (this.y > height - (this.h/2)|| this.y < 0 + (this.h/2)) {
       if (this.y > height - (this.h/2)) {
-       this.y = height - (this.h/2);
+        this.y = height - (this.h/2);
+        this.v = 0
+        this.v_hor = 0
       } else if ( this.y < 0 + (this.h/2)){
         this.y = 0 + (this.h/2);
+        this.v = 0
       }
     }
 
     if (this.x > width - (this.w/2)|| this.x < 0 + (this.w/2)) {
       if (this.x > height - (this.w/2)) {
-       this.x = width - (this.w/2);
+        this.x = width - (this.w/2);
+        this.v_hor = 0
       } else if ( this.x < 0 + (this.w/2)){
         this.x = 0 + (this.w/2);
+        this.v_hor = 0
       }
     }
   }
@@ -70,6 +76,7 @@ class Block{
     if (hit == true){
       this.c = "red"
       character.v = 0
+      character.v_hor = 0
       character.y = this.y - (this.h/2) 
     } else {
       this.c = "white"
@@ -90,7 +97,7 @@ var blocks = [], hit = false
 function setup() {
   createCanvas(550, 500);
   character = new Character(150,150,50,50, "white");
-  block = new Block(475,450,50,50, "white");
+  block = new Block(100,450,50,50, "white");
 
 }
 
@@ -108,9 +115,20 @@ function draw() {
 
 
 function keyPressed() {
-  if (keyCode == 32) {
-    character.v = -10
-  }
+  if (keyIsDown(LEFT_ARROW)) {
+    if (keyCode == 32) {
+      character.v = -10
+      character.v_hor = -5
+    }
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    if (keyCode == 32) {
+      character.v = -10
+      character.v_hor = 5
+    }
+  } else if (keyCode == 32) {
+      character.v = -10
+    }
+  
 }
 
     
