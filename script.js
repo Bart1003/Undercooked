15502d5f08015692c80c4bc8bc877cdf277f0522
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2022-01-26 13:47:49 +0000

    een jump voor de bal tegevoegd als je op spatie klinkt

--- script.js
+++ script.js
@@ -4,20 +4,37 @@ class Character{
     this.y = y;
     this.w = w;
     this.h = h;
-    this.c = color
-    this.v = 3
-    this.a = 1.05
+    this.c = color;
+    this.v = 1;
+    this.a = 0.33; 
   }
+
+  
+  jump(){
+
+  }
+
+
 
   draw(){
     fill(this.c)
     ellipse(this.x, this.y, this.w, this.h);
 
-    if (this.v < 10){
-      this.v = this.v * this.a;
-    }
-    this.y += this.v
+    
+    this.y += this.v;
+    this.x += 2
 
+    if (this.v < 15){
+        this.v = this.v + this.a;
+      } 
+    
+
+    //helps see what happens to the velocity, not a part of the end game (at least not planned yet, could be fun though)
+    textSize(32)
+    fill(50)
+    text(this.v, 100, 30);
+
+    //collision met de grond en plafond
     if (this.y > height - (this.h/2)|| this.y < 0 + (this.h/2)) {
       if (this.y > height - (this.h/2)) {
        this.y = height - (this.h/2);
@@ -25,7 +42,17 @@ class Character{
         this.y = 0 + (this.h/2);
       }
     }
+
+    if (this.x > width - (this.w/2)|| this.x < 0 + (this.w/2)) {
+      if (this.x > height - (this.w/2)) {
+       this.x = width - (this.w/2);
+      } else if ( this.x < 0 + (this.w/2)){
+        this.x = 0 + (this.w/2);
+      }
+    }
   }
+
+
 
 }
 
@@ -35,13 +62,15 @@ class Block{
     this.y = y;
     this.w = w;
     this.h = h;
-    this.c = color
+    this.c = color;
   }
 
   hit(){
     hit = collideRectCircle(this.x,this.y,this.w, this.h , character.x, character.y, character.w, character.h);
     if (hit == true){
       this.c = "red"
+      character.v = 0
+      character.y = this.y - (this.h/2) 
     } else {
       this.c = "white"
     }  
@@ -56,27 +85,32 @@ class Block{
 }
 
 
-var blocks = [], hit = false;
+var blocks = [], hit = false
 
 function setup() {
   createCanvas(550, 500);
   character = new Character(150,150,50,50, "white");
-  block = new Block(100,450,50,50, "white");
+  block = new Block(475,450,50,50, "white");
 
 }
 
 function draw() {
 	background(225);  
   
-  block.hit();
-  character.draw();
-  block.draw();
   
+  character.draw();
+  block.hit();
+  block.draw();
 
   
 }
 
 
 
+function keyPressed() {
+  if (keyCode == 32) {
+    character.v = -10
+  }
+}
 
     
