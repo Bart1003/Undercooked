f835ffe97a501604a435fc9f3ef05a2dcdaea6e0
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2022-01-26 17:50:36 +0000

    naam van variabel v naar v_ver verandered voor extra duidelijkheid

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Character{
     this.w = w;
     this.h = h;
     this.c = color;
-    this.v = 1;
+    this.v_ver = 1;
     this.a = 0.33; 
     this.v_hor = 1
   }
@@ -22,28 +22,28 @@ class Character{
     ellipse(this.x, this.y, this.w, this.h);
 
 
-    if (this.v < 15){
-        this.v = this.v + this.a;
+    if (this.v_ver < 15){
+        this.v_ver = this.v_ver + this.a;
       } 
-    this.y += this.v;
-    
+
+    this.y += this.v_ver;
     this.x += this.v_hor
 
     //helps see what happens to the velocities, not a part of the end game (at least not planned yet, could be fun though)
     textSize(32)
     fill(50)
-    text(this.v, 100, 30);
+    text(this.v_ver, 100, 30);
     text(this.v_hor, 100, 70)
 
     //collision met de grond en plafond
     if (this.y > height - (this.h/2)|| this.y < 0 + (this.h/2)) {
       if (this.y > height - (this.h/2)) {
         this.y = height - (this.h/2);
-        this.v = 0
+        this.v_ver = 0
         this.v_hor = 0
       } else if ( this.y < 0 + (this.h/2)){
         this.y = 0 + (this.h/2);
-        this.v = 0
+        this.v_ver = 0
       }
     }
 
@@ -75,7 +75,7 @@ class Block{
     hit = collideRectCircle(this.x,this.y,this.w, this.h , character.x, character.y, character.w, character.h);
     if (hit == true){
       this.c = "red"
-      character.v = 0
+      character.v_ver = 0
       character.v_hor = 0
       character.y = this.y - (this.h/2) 
     } else {
@@ -117,18 +117,18 @@ function draw() {
 function keyPressed() {
   if (keyIsDown(LEFT_ARROW)) {
     if (keyCode == 32) {
-      character.v = -10
+      character.v_ver = -10
       character.v_hor = -5
     }
   } else if (keyIsDown(RIGHT_ARROW)) {
     if (keyCode == 32) {
-      character.v = -10
+      character.v_ver = -10
       character.v_hor = 5
     }
   } else if (keyCode == 32) {
-      character.v = -10
+      character.v_ver = -10
     }
-  
+
 }
 
     
