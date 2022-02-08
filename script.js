3c312213ca3931b0e859376acc85abf535e1cb94
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2022-02-08 18:58:43 +0000

    smooth screenscroll die meebeweegt met je character

--- script.js
+++ script.js
@@ -37,24 +37,29 @@
     rect(this.x, this.y, this.w, this.h);
 
 
-    if (this.v_ver < this.v_ver_max){
-      this.v_ver = this.v_ver + this.a;
-    } 
+    //if (this.v_ver < this.v_ver_max){
+      //this.v_ver = this.v_ver + this.a;
+    //} 
   
-    this.y += this.v_ver
+    //this.y += this.v_ver
     this.x += this.v_hor
 
+    
+
+
+    
     this.collision = checkCollision()
     if (this.collision == "top"){
-      this.v_ver = 0
+      //this.v_ver = 0
       //this.v_hor = 0
     } else if (this.collision == "left" || this.collision == "right"){
       this.v_hor = this.v_hor * -this.bounce
       this.collision = false
     } else if (this.collision == "bottom"){
-      this.v_ver = 0
+      //this.v_ver = 0
       this.v_hor = 0
     }
+    
 
     
 
@@ -65,24 +70,25 @@
     textSize(32)
     fill(50)
     
-    text(this.collision, 100, 100)
-    text(this.v_ver, 100, 30);
+    //text(this.collision, 100, 100)
+    //text(this.v_ver, 100, 30);
     text(this.v_hor, 100, 70)
 
     //collision met de grond en plafond
-    if (this.y > height - this.h || this.y < 0) {
-      if (this.y > height - this.h) {
-        this.y = height - this.h;
-        this.v_ver = 0
-        this.v_hor = 0
-      } else if (this.y < 0) {
-        this.y = 0;
-        this.v_ver = 0
-      }
-    }
+    //if (this.y > height - this.h || this.y < 0) {
+      //if (this.y > height - this.h) {
+        //this.y = height - this.h;
+        //this.v_ver = 0
+        //this.v_hor = 0
+      //} 
+      //if (this.y < 0) {
+        //this.y = 0;
+        //this.v_ver = 0
+      //}
+    //}
 
-    if (this.x > width - this.w || this.x < 0 + (this.w/2)) {
-      if (this.x > height - this.w) {
+    if (this.x > width - this.w || this.x < 0 + (this.w)) {
+      if (this.x > width - this.w) {
         this.x = width - this.w;
         this.v_hor = this.v_hor * -this.bounce
       } else if (this.x < 0){
@@ -102,12 +108,37 @@ class Block{
     this.c = color;
     this.halfWidth = this.w /2; //variable for collision checking
     this.halfHeight = this.h /2; //variable for collision checking
+    this.v_ver = 0
+    this.v_ver_max = 15
+    this.a = 0.33
+    this.collision = false
   }
 
 
   draw(){
     fill(this.c)
     rect(this.x, this.y, this.w, this.h);
+
+    if (this.v_ver < this.v_ver_max){
+      this.v_ver = this.v_ver + this.a;
+    } 
+
+    
+    this.y -= this.v_ver
+
+    fill(50)
+    text(this.v_ver, 100, 30);
+    text(character.collision, 100, 100)
+    if (character.collision == "top"){
+      this.v_ver = 0
+    } else if (character.collision == "bottom"){
+      this.v_ver = 0
+    }
+    
+    
+    
+
+    
   }
   
 }
@@ -118,10 +149,9 @@ max_jump_height = 15, min_jump_height = 2 //min and max height the character can
 ver_jump_speed = 7.5 //speed when jumping vertically
 
 function setup() {
-  createCanvas(550, 500);
-  character = new Character(150,150,50,50, "white");
-  block = new Block(300,250,100,50, "white");
-  blocks = [new Block(150,250,300,50, "white"), new Block(350,100,50,20, "white"), new Block(0,300,50,200, "white"), new Block(0,height,width,0.1, "white")] 
+  createCanvas(550, 750);
+  character = new Character(150,450,50,50, "white");
+  blocks = [new Block(150,(height-250),300,50, "white"), new Block(350,(height-400),50,20, "white"), new Block(0,(height-200),50,200, "white"), new Block(0,height,width,10, "white")] 
 
 }
 
@@ -130,11 +160,13 @@ function draw() {
   
   
   
-  character.jump_walk()
-  character.draw();
- // block.hit2();
+  
+  
  
   blocks.forEach(b => b.draw())
+  character.jump_walk()
+  character.draw();
+  
   
 
   
@@ -143,15 +175,15 @@ function draw() {
 function keyReleased(){
   if (keyCode == 32 && character.collision == "bottom"){
     if (character.jump_time > max_jump_height){
-      character.v_ver = -max_jump_height
+      blocks.forEach(b => b.v_ver = -max_jump_height)
     } else if (character.jump_time < min_jump_height) {
-      character.v_ver = -min_jump_height
+      blocks.forEach(b => b.v_ver = -min_jump_height)
     } else {
-      character.v_ver = -character.jump_time
+      blocks.forEach(b => b.v_ver = -character.jump_time)
     }
     character.jump_time = 0
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
-    character.v_hor = -ver_jump_speed
+      character.v_hor = -ver_jump_speed
     } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
       character.v_hor = ver_jump_speed
     }
@@ -191,11 +223,14 @@ function checkCollision(){
         // collision is on the smallest overlap
         if(overlapX >= overlapY){
           if(dy > 0) {
-            character.y += overlapY;
+            blocks.forEach(b => b.y -= overlapY) 
+
+            //character.y += overlapY;
             colliding = "top";
           }
-          else {            
-            character.y -= overlapY;
+          else { 
+            blocks.forEach(b => b.y += overlapY)           
+            //character.y -= overlapY;
             colliding = "bottom";            
           }
         }
