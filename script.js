19853ce8eb626914c2b599af86aa838b033e4024
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2022-02-21 12:34:52 +0000

    a en d toetsen werkt nu ook met loop animatie

--- script.js
+++ script.js
@@ -443,7 +443,7 @@ function pickImage(){
   if (character.walking == true){
     frame_counter += 1
 
-    if (keyIsDown(RIGHT_ARROW)){
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)){
       if (frame_counter >= 1 && frame_counter <= 10){
         character.img = charrun1right
       } else if (frame_counter >= 11 && frame_counter <= 20){
@@ -455,7 +455,7 @@ function pickImage(){
       }
     }
 
-    if (keyIsDown(LEFT_ARROW)){
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(65)){
       if (frame_counter >= 1 && frame_counter <= 10){
         character.img = charrun1left
       } else if (frame_counter >= 11 && frame_counter <= 20){
